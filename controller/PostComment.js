ac14cb0543af16a81aafcac75bdcb62d5c7f84b2
cpa4001 <cpapostoli4001@eagle.fgcu.edu>
2022-09-25 21:19:08 -0400

    created comment model and get and post files

---
+++ controller/PostComment.js
@@ -0,0 +1,36 @@
+const Comment = require('../models/comment')
+
+postComment = (req, res) => {
+  const body = req.body
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a body for the comment',
+    })
+  }
+
+  const comment = new Comment(body)
+
+  if (!comment) {
+    return res.status(400).json({ success: false, error: err })
+  }
+
+  comment
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: blog._id,
+        message: 'Comment created!',
+      })
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error,
+        message: 'Comment not created!',
+      })
+    })
+}
+
+module.exports = { postComment }
